759c40dc4dbfc00adb50089d19d2ba4ee04626a0
volta-v <64754632+volta-v@users.noreply.github.com>
2020-06-02 19:59:30 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+	const st = document.querySelector("#st")
+	const hp = document.querySelector("#hp")
+	const tlotr = document.querySelector("#tlotr")
+	const b99 = document.querySelector("#b99")
+	const pp = document.querySelector("#pp")
+	const pr = document.querySelector("#pr")
+	
+	st.onclick = function() {
+    document.getElementById("central").src = "https://i.imgur.com/r9f1wRS.jpg";   
+    }
+
+  hp.onclick = function() {
+    document.getElementById("central").src = "https://i.imgur.com/rFh9N84.jpg";   
+    }
+
+  tlotr.onclick = function() {
+    document.getElementById("central").src = "https://i.imgur.com/UyKVPES.jpg";   
+    }
+
+  b99.onclick = function() {
+    document.getElementById("central").src = "https://i.imgur.com/z1plpdx.jpg";   
+    }
+
+   pp.onclick = function() {
+    document.getElementById("central").src = "https://i.imgur.com/YsTmO6e.jpg";   
+    }
+
+	 pr.onclick = function() {
+    document.getElementById("central").src = "https://i.imgur.com/IWxOH20.jpg";   
+    }
+
+
+}
+
